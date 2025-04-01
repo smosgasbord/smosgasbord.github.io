a24526db8040f3a57f268bf51335865c224296f6
smosgasbord <saenz.ran@gmail.com>
2025-04-01 01:27:56 -0600

    claridad

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#2d4654',
+        sage: '#5e8b7e',
+        turquoise: '#7fd1c7',
+        cream: '#f5f5f0',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
   plugins: [require('daisyui')],
 };
